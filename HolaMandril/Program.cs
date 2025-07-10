cf1602fad03b21dc9392ce9e1ff196e92fc445aa
user <user@LAPTOP-ER>
2025-07-10 18:33:00 -0500

    Add project files.

---
+++ HolaMandril/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("03. Hola Real Mandril!");
+
+Console.WriteLine("01. Hola Real Mandril!");
+
+Console.WriteLine("02. Hola Real Mandril!");
