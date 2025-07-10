6577ca08b1d1304bf89f8314d6d4e42bd221d97f
user <user@LAPTOP-ER>
2025-07-10 18:38:32 -0500

    Probando GIT

--- HolaMandril/Program.cs
+++ HolaMandril/Program.cs
@@ -4,3 +4,5 @@ Console.WriteLine("03. Hola Real Mandril!");
 Console.WriteLine("01. Hola Real Mandril!");
 
 Console.WriteLine("02. Hola Real Mandril!");
+
+Console.WriteLine("Probando el uso de GIT");
