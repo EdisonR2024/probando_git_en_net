47a645b4c00a9aaf7a335b566fa9273668cf7ce4
user <user@LAPTOP-ER>
2025-07-11 13:16:57 -0500

    Creando clase Persona

--- HolaMandril/Program.cs
+++ HolaMandril/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using HolaMandril.Entidades;
+
 Console.WriteLine("03. Hola Real Mandril!");
 
 Console.WriteLine("01. Hola Real Mandril!");
@@ -6,3 +8,14 @@ Console.WriteLine("01. Hola Real Mandril!");
 Console.WriteLine("02. Hola Real Mandril!");
 
 Console.WriteLine("Probando el uso de GIT");
+
+// Instancia de la clase Persona
+Persona persona = new Persona
+{
+    Id = 1,
+    Nombre = "Juan",
+    Apellido = "Pérez",
+    Edad = 30
+};
+
+Console.WriteLine($"Id: {persona.Id}, Nombre: {persona.Nombre}, Apellido: {persona.Apellido}, Edad: {persona.Edad}");
